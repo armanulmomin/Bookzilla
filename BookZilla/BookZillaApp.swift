fa6239516fcb6fd74cff2b00615b78aff214bd99
Armanul Momin <armanul.momin@gmail.com>
2025-07-25 21:12:38 +1000

    first Commit

---
+++ BookZilla/BookZillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookZillaApp.swift
+//  BookZilla
+//
+//  Created by Arman on 25/7/25.
+//
+
+import SwiftUI
+
+@main
+struct BookZillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
